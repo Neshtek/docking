c833a4e529822f1f2438653604f74607f5e3ac2c
Gaurav0528 <73791921+Gaurav0528@users.noreply.github.com>
2023-04-09 11:42:13 +0530

    updated gaurav

--- src/util.py
+++ src/util.py
@@ -3,34 +3,20 @@ import sys
 import os
 
 def create_data_map(array):
-    outlier = []
-    dict = {}
-    mean_x = (np.average(array[0]))
-    mean_y = (np.average(array[1]))
-
-    center_p = np.array((mean_x, mean_y))
-
-    for i in range(len(array[0])):
-        point = np.array((array[0][i], array[1][i]))
-        point_tuple = (array[0][i], array[1][i])
-        distance = np.linalg.norm(center_p - point)
-        data = {point_tuple: distance}
-        dict.update(data)
-
-    if len(dict) > 2:
-        val_list = list(dict.values())
-        key_list = list(dict.keys())
-        mean = np.mean(val_list)
-        sd = np.std(val_list)
-        for i in range(len(val_list)):
-            z = (val_list[i] - mean) / sd
-            if z > 0.5 or z < -0.5:
-                key = key_list[i]
-                array[0].remove(key[0])
-                array[1].remove(key[1])
-                #print(key[0])
-                #outlier.append(key)
-        #return outlier
+#     start_time = time.time()
+    median = np.median(data)
+    mad = np.median(np.abs(data - median))
+    threshold = 3 * mad
+    diff = np.abs(data - median)
+    z = diff / mad if mad else 0.0
+    outliers = np.nonzero(z > threshold)[0]
+    
+#     end_time = time.time()
+#     print("Execution time: ", end_time - start_time,"secs")
+    if len(outliers) == 0:
+        return data
+    else:
+        return np.take(data, np.delete(np.arange(len(data)), outliers), axis=0)
     
 def keyboard_shutdown():
     print('Interrupted\n')
